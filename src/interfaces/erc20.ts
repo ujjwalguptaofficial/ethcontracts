86733cfd1e3e1d320ad02722db302ff1b57022ce
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-11-24 20:52:05 +0530

    add transferFrom method in ierc20

--- src/interfaces/erc20.ts
+++ src/interfaces/erc20.ts
@@ -9,5 +9,10 @@ export interface IERC20 {
     getTotalSupply<T>(): Promise<T>;
     getAllowance<T>(owner: string, spender: string): Promise<T>;
     approve(spender: string, amount, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>;
-    transfer(to: string, amount, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>
+    transfer(to: string, amount, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>;
+    transferFrom(from: string, to: string, amount, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>;
+
+    increaseAllowance(spender: string, addedValue, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>;
+
+    decreaseAllowance(spender: string, subtractedValue, tx?: ITransactionRequestConfig): Promise<TYPE_TRANSACTION_WRITE_RESULT>
 }
