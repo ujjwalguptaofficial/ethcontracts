6c1440afcbe3108ff2624939e34155d6e65288a2
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-11-26 12:56:42 +0530

    add doc link

--- test/debug.js
+++ test/debug.js
@@ -6,15 +6,8 @@ const abi = require("../abi/erc20.json");
 const token = new ERC20('0x8f3cf7ad23cd3cadbd9735aff958023239c6a063');
 
 async function init() {
-    const provider = new providers.StaticJsonRpcProvider('https://polygon-rpc.com');
+    const provider = new providers.JsonRpcProvider('https://polygon-rpc.com');
 
-    // const ct = new Contract(token.tokenAddress,abi,provider);
-    // console.log(ct);
-    // const b = await ct.balanceOf('0xd5D3F35Bdd08950CCFE0DeAb638F8B5498297076');
-    // console.log(b);
-    // return;
-
-    console.log('issigner', provider, 'd');
 
     // const provider = new Web3.providers.HttpProvider('https://polygon-rpc.com');
     const client = new EthersClient(provider);
